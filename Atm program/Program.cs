751841e86f130d254aaf136a4fecf9bc56f212b1
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-15 10:44:56 +0530

    Add files via upload

---
+++ Atm program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ATM
+{
+    public static void Main(string[] args)
+    {
+        int amount = 2560;
+        int[] notes = { 2000, 1000, 500, 200, 100, 50, 10 };
+        foreach (int note in notes)
+        {
+            int count = amount / note;
+            if (count > 0) {
+                Console.WriteLine("Rs." + note + " : " + count + " note");
+                amount %= note;
+            }
+        }
+        Console.ReadKey();
+       
+    }
+
+    
+}
