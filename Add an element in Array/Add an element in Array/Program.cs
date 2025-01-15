caf389db88fa4992b0c6e559f66c45ff0bcbf24b
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-15 10:41:01 +0530

    Add files via upload

---
+++ Add an element in Array/Add an element in Array/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_an_element_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            int element = 4;
+            int[] newarr = new int[arr.Length + 1];
+
+            Array.Copy(arr, newarr, arr.Length);
+
+            newarr[arr.Length] = element;
+            Console.WriteLine("After adding new element array is :");
+            foreach (int i in newarr)
+            {
+                Console.Write(i+" ");
+            }
+
+
+        }
+    }
+}
