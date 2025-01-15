dfb02cc9346d4ea5db750c1a02d6e177a932c1e8
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-15 10:43:59 +0530

    Add files via upload

---
+++ Addition using Recursion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition_using_Recursion
+{
+    public class Program
+    {
+        public static int Add(int a = 5, int b = 4)
+        {
+            if (a == 0)  
+            return b;
+        
+            return Add(--a, ++b);          
+        }
+         public static void Main(string[] args)
+        {
+            int sum = Add();
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
