d2a4d49812a841031cbb0d735a79b639542f7613
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-17 10:16:51 +0530

    Add files via upload

---
+++ Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int fact = 1;
+            Console.WriteLine("Enter Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of Number : "+num+" is "+fact);
+        }
+    }
+}
