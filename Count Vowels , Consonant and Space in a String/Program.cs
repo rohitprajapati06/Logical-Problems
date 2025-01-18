694d97212147ffd09d74f11f2eff64114962421b
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-18 13:22:36 +0530

    Add files via upload

---
+++ Count Vowels , Consonant and Space in a String/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Vowels___Consonant_and_Space_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vowels = 0, consonants = 0 , spaces = 0;
+            Console.WriteLine("Enter a String");
+            string input = Console.ReadLine().ToLower();
+
+            foreach(char c in input)
+            {
+                if ("aeiou".Contains(c))
+                {
+                    vowels++;
+                }
+                else if (Char.IsLetter(c)) { 
+                    consonants++;
+                }
+                else if(c ==' '){
+                    spaces++;   
+                }
+            }
+
+            Console.WriteLine("Vowels : " + vowels + "\nConsonants : " + consonants + "\nSpaces : " + spaces);
+
+        }
+    }
+}
