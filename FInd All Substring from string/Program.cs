02ad4a47cd3b55faa048162aca8c255d2c546c69
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-18 13:24:54 +0530

    Add files via upload

---
+++ FInd All Substring from string/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "abc"; // Example string
+        Console.WriteLine("All substrings of "+input);
+
+        FindAllSubstrings(input);
+    }
+
+    static void FindAllSubstrings(string input)
+    {
+        int length = input.Length;
+
+        // Outer loop for the starting point of substrings
+        for (int i = 0; i < length; i++)
+        {
+            // Inner loop for the ending point of substrings
+            for (int j = i + 1; j <= length; j++)
+            {
+                // Extract substring using Substring method
+                Console.WriteLine(input.Substring(i, j - i));
+            }
+        }
+    }
+}
+
+
+/* Substring copy the string from the input.Substring(start position , end position)*/
