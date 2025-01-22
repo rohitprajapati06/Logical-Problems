f3ff65b608fb77b2ba21e4a00a5f2e1122c038a4
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-22 10:49:24 +0530

    Add files via upload

---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length_of_Last_Word
+{
+    internal class Program
+    {
+        public static int Length(string s)
+        {
+            string[] s2 = s.Trim().Replace("  "," ").Split(' ');
+            string n = s2[s2.Length - 1];
+            return n.Length;
+        }
+        static void Main(string[] args)
+        {
+            string s = " we  are flying in  the  moon  ";
+            int length = Length(s);
+            Console.WriteLine("The Length of the last word : "+length);
+
+            Console.ReadKey();
+        }
+    }
+}
