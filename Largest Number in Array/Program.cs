21bcf2a85d314a4f37a16e6ba23a271b6210d2dd
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-22 10:47:30 +0530

    Add files via upload

---
+++ Largest Number in Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_Number_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 58,3,54,69,89,87,94,4,32,91 };
+            int largestnum = LargestNumber(arr);
+            Console.WriteLine("The Largest Number in an Array is : "+largestnum);
+            Console.ReadKey();
+
+        }
+
+        public static int LargestNumber(int[] arr)
+        {
+            int largest = int.MinValue;
+
+            foreach (int num in arr) 
+            {
+                if(num > largest)
+                {
+                    largest = num;
+                }
+            }
+            return largest;
+        }
+    }
+}
