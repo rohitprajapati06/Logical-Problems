66ba56a9059bc782730b5c4fad5b3b0dcfd33afe
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-22 10:45:33 +0530

    Add files via upload

---
+++ Find the Average of an Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_Average_of_an_Array
+{
+    internal class Program
+    {
+        public static int Average(int[] arr) {
+
+            int length = arr.Length;
+            int sum = 0;
+            for (int i = 0; i <= length-1; i++) { 
+            
+                sum += arr[i];
+            }
+            int average = sum / length;
+            return average;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = {10,2,20,4};
+            int avg = Average(arr);
+            Console.WriteLine("Average of an Array : "+avg);
+        }
+    }
+}
