e24804adf1abcac020780011cfe3e0399f3daafb
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-23 17:40:48 +0530

    Add files via upload

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class PalindromeExample
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the Number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int r, sum = 0, temp;
+
+        temp = n;
+        while (n > 0)
+        {
+            r = n % 10;
+            sum = sum * 10 + r;
+            n = n / 10;
+        }
+        if (temp == sum)
+            Console.Write("Number is Palindrome.");
+        else
+            Console.Write("Number is not Palindrome");
+    }
+}
