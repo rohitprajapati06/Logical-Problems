0f96d7cda0ac5a64fe8bef927bbdfba16945d31d
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-23 10:29:03 +0530

    Add files via upload

---
+++ Maximum Product Subarray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    public static int MaxProductSubarray(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+            return 0;
+
+        int maxProduct = nums[0];
+        int currentMax = nums[0];
+        int currentMin = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < 0)
+            {
+                // Swap currentMax and currentMin
+                int temp = currentMax;
+                currentMax = currentMin;
+                currentMin = temp;
+            }
+
+            currentMax = Math.Max(nums[i], currentMax * nums[i]);
+            currentMin = Math.Min(nums[i], currentMin * nums[i]);
+
+            maxProduct = Math.Max(maxProduct, currentMax);
+        }
+
+        return maxProduct;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = { 2, 3, -2, 4 ,-1};
+        Console.WriteLine("Maximum Product Subarray: " + MaxProductSubarray(nums));
+    }
+}
