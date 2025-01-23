d3a1761bcc6f3f7e401dd45341beec18fa81b956
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-23 10:30:31 +0530

    Add files via upload

---
+++ Merge Two Sorted Array/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Two_Sorted_Array
+{
+    public class Program
+    {
+        public static int[] MergeSortedArray(int[] arr1 , int[] arr2)
+        {
+            List<int> merged = new List<int>();
+            int i = 0 , j = 0 ;
+            while(i < arr1.Length && j < arr2.Length)
+            {
+                if (arr1[i] < arr2[j]) {
+                    merged.Add(arr1[i]);
+                    i++;
+                }
+                else
+                {
+                    merged.Add(arr2[j]);
+                    j++;
+                }
+            }
+
+            while(i < arr1.Length)
+            {
+                merged.Add(arr1[i]);
+                i++;
+            }
+
+            while(j < arr2.Length)
+            {
+                merged.Add(arr2[j]);
+                j++;
+            }
+
+            return merged.ToArray();
+        } 
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 3, 6 };
+            int[] arr2 = { 2,4, 5 };
+            int[] MergeArray = MergeSortedArray( arr1, arr2);
+
+            Console.WriteLine(string.Join(" , ",MergeArray));
+            Console.ReadKey();
+        }
+    }
+}
