26e307f3f0a266eefc2ccd383091bb26e3e0f2ee
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-24 19:17:02 +0530

    Add files via upload

---
+++ Recursion - Multiples & 1 to 100/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion___Multiples___1_to_100
+{
+    internal class Program
+    {
+        static void  Mult(int number)
+        {
+            if(number > 100)    //If number is grater than 100 , recursion will stop
+                return;
+
+            Console.WriteLine(number); // This will print the number
+            Mult(number + 8);   // Recursive Function call itself untill number exceeds 100
+        }
+            
+
+        static void Main(string[] args)
+        {
+            Mult(8);
+            Console.ReadKey();
+        }
+    }
+}
