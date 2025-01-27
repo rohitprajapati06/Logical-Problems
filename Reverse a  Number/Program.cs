0d1af607daa2c0f9c3b0f4ec778871961e8054c4
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-27 15:05:54 +0530

    Add files via upload

---
+++ Reverse a  Number/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, rev = 0 , rem,temp;
+            Console.WriteLine("Enter number for Reverse");
+             n = Convert.ToInt32(Console.ReadLine());
+            while(n > 0)
+            {
+                rem = n % 10;
+                rev = rev * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reverse of a number is "+rev);
+        }
+    }
+}
