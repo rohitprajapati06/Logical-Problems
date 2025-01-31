8716cbac92a90d7f069c2106143e05a497959b87
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-01-31 11:59:55 +0530

    Add files via upload

---
+++ Search an Element in Array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_an_Element_in_Array
+{
+    internal class Program
+    {
+        public static int LinearSearch(int[] arr, int target)
+        {
+            for(int i = 0 ; i <arr.Length ; i++) {
+
+                if (arr[i] == target) { 
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 6, 8, 3, 9 };
+            Console.WriteLine("Enter the element to search for");
+            int target = Convert.ToInt32(Console.ReadLine());
+
+            int index = LinearSearch(arr, target);
+            if (index != -1)
+            {
+                Console.WriteLine("Element " + target + " found at index :" + index);
+            }
+            else {
+                Console.WriteLine("Element " + target + " not found in  Array ");
+            }
+        }
+    }
+}
