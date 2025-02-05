107a9d7410f8d92551a521055ec1f701f94e1979
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-02-05 23:55:05 +0530

    Add files via upload

---
+++ Seperate string from letters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = "Hello123";
+        string letters = "";
+
+        foreach(char c in input)
+        {
+            if ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+            { 
+                letters += c;
+            }
+        }
+        Console.WriteLine("Orignal Strings : "+input);
+        Console.WriteLine("Seperated Strings : "+letters);
+        Console.ReadKey();
+    }
+}
+
+
+
+
+/*              Logic
+        
+ ASCII Range for Letters:
+'A' to 'Z': Uppercase letters.
+'a' to 'z': Lowercase letters.
+
+        foreach(char c in input)
+        {
+            if ( (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+            { 
+                letters += c;
+            }
+        }
+
+In this it will check through range of ASCII letters and add it in var letters
+ 
+ */
