e03cbb99ad2edf39c28776d93a25e15c221549cc
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-02-05 23:54:04 +0530

    Add files via upload

---
+++ Seperate Numbers from strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seperate_Numbers_from_strings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = "Hello123";
+            string nums = "";
+
+            foreach(char c in input)
+            {
+                if (c >= '0' && c <= '9') 
+                {              
+                    nums += c;
+                }
+            }
+            Console.WriteLine("Orignal Strings : "+input);
+            Console.WriteLine("Seperated Numbers : "+nums);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
