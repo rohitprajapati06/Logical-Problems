0045b1169ff7816bf825a9094aa955cbed4aba3d
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-02-08 19:45:28 +0530

    Add files via upload

---
+++ Sum of Digit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0, remainder;
+
+            while (num > 0) {
+
+                remainder = num % 10;
+                sum = sum + remainder;
+                num = num/10;
+            }
+            Console.WriteLine("Sum of Digits are :" + sum);
+            Console.ReadKey();
+        }
+    }
+}
