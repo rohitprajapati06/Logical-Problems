7803a8778c09d2deb2168cbed3147763ccc35032
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-02-08 19:43:23 +0530

    Add files via upload

---
+++ Smallest Number in Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smallest_Number_in_Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 54,62,35,15,36,12,16,11,78,95};
+            int smallnum = SmallestArray(arr);
+            Console.WriteLine("Smaller Number in an Array is : "+smallnum);
+            Console.ReadKey();
+        }
+
+        public static int SmallestArray(int[] arr) {
+            
+            int smallest = int.MaxValue;
+            foreach (int num in arr) { 
+                if(num < smallest)
+                {
+                    smallest = num; 
+                }
+            }
+            return smallest;
+        }
+    }
+}
