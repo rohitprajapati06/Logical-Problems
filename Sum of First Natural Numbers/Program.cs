64681775de61f7df05891997b99a0dbd6c560d48
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-02-09 17:40:08 +0530

    Add files via upload

---
+++ Sum of First Natural Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_First_Natural_Numbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int n = SumofNaturalNumbers(num);
+            Console.WriteLine("Sum of N Natural Number till " + num + " is " + n);
+
+        }
+
+        public static int SumofNaturalNumbers(int num)
+        {
+            return num * (num + 1) / 2;
+        }
+    }
+}
