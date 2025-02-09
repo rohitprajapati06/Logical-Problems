0b4f6d7a8f14cb6e12df841a6db67793f7da0e2b
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-02-09 17:40:35 +0530

    Add files via upload

---
+++ Sum of Squares of Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Squares_of_Numbers
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the number of terms (N): ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = SumOfSquares(n);
+            Console.WriteLine("The sum of the series 1^2 + 2^2 + ... + N^2 is : "+sum);
+        }
+
+        public static int SumOfSquares(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i * i;
+            }
+            return sum;
+        }
+    }
+
+}
