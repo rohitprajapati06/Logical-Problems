23cdccea739c60226005e4fde24803ef2dd7848d
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-02-12 22:50:49 +0530

    Add files via upload

---
+++ Highest Odd Number/Highest Odd Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Highest_Odd_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "54616792";
+            for (int i = input.Length - 1; i >= 0; i--) {
+
+                if (input[i] % 2 != 0)
+                {
+                    Console.WriteLine($" Highest Odd Number : {input.Substring(0, i + 1)}");
+                    return;
+                }
+            }
+        }
+    }
+}
