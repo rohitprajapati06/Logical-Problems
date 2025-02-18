b8827d70a3ea0073f27b41b17ee2e233da344aec
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-02-18 14:37:43 +0530

    Add files via upload

---
+++ Left Shift using Recursion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void LeftShift(int[] arr, int index, int start)
+        {
+
+            if (index == arr.Length - 1)
+            {
+                arr[index] = start;
+                return;
+            }
+
+            arr[index] = arr[index + 1];
+
+            LeftShift(arr, index + 1, start); 
+
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int start = arr[0];
+            LeftShift(arr, 0, start);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
