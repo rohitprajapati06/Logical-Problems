1e4ee18c6963c67b54747162625d5b5fbc8b1426
Rohit Prajapati <113875720+rohitprajapati06@users.noreply.github.com>
2025-03-06 12:49:47 +0530

    Add files via upload

---
+++ ArmStrong Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmStrong_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int n = int.Parse(Console.ReadLine());
+
+            int temp = n , sum = 0 , rem = 0 ;
+
+            while (n > 0) {
+                rem = n % 10;
+                sum = sum + (rem * rem * rem);
+                n = n / 10;
+            }
+
+            if(temp == sum)
+            {
+                Console.WriteLine($"{sum} is an armstrong number");
+            }
+            else
+            {
+                Console.WriteLine($"{temp} is not an armstrong number");
+            }
+        }
+    }
+}
